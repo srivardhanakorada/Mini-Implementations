b3eafa6cfca1ff2692a3e2a584a6a115696a42af
vardhankorada <vardhankorada@gmail.com>
2025-09-25 22:34:34 +0530

    cnn_cifar

--- nets/cnn_cifar.py
+++ nets/cnn_cifar.py
@@ -1,10 +1,8 @@
 ## Imports
 import torch
-from torchvision.datasets import MNIST
 from torchvision import transforms
-from torch.utils.data import random_split, DataLoader
+from torch.utils.data import random_split, DataLoader, Dataset
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import random
@@ -14,9 +12,9 @@ from torch.utils.tensorboard import SummaryWriter
 ## Setup
 SEED = 42
 BATCH_SIZE = 128
-IMAGE_HEIGHT = 28
-IMAGE_WIDTH = 28
-IMAGE_CHANNELS = 1
+IMAGE_HEIGHT = 32
+IMAGE_WIDTH = 32
+IMAGE_CHANNELS = 3
 NUM_CLASSES = 10
 NUM_EPOCHS = 10
 DEVICE = "cuda:0"
@@ -29,37 +27,65 @@ def set_seed(seed):
 set_seed(SEED)
 
 ## Data
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+class CIFAR_Dataset(Dataset):
+    def __init__(self,path,transform,num_splits=1):
+        batches = [unpickle(f"{path}/data_batch_{i}") for i in range(1,num_splits+1)]
+        x = [batches[i][b'data'] for i in range(0,len(batches))]
+        y = [batches[i][b'labels'] for i in range(0,len(batches))]
+        self.x = np.vstack(np.asarray(x))
+        self.x = self.x.reshape(-1,IMAGE_CHANNELS,IMAGE_WIDTH,IMAGE_HEIGHT)
+        self.x = np.transpose(self.x,(0,2,3,1))
+        self.y = np.hstack(np.asarray(y))
+        self.transform = transform
+    def __len__(self): return self.x.shape[0]
+    def __getitem__(self, idx):
+        img = self.x[idx]
+        label = self.y[idx]
+        if self.transform: img = self.transform(img)
+        return img,label
 tfm = transforms.Compose([transforms.ToTensor()])
-train_full_data = MNIST(root="data/MNIST",train=True,transform=tfm,download=True)
+train_full_data = CIFAR_Dataset(path="data/cifar-10-batches-py/train",transform=tfm,num_splits=5)
+test_data = CIFAR_Dataset(path="data/cifar-10-batches-py/test",transform=tfm,num_splits=1)
 train,val = random_split(train_full_data,[0.8,0.2],generator=torch.Generator().manual_seed(SEED))
 train_loader = DataLoader(train,batch_size=BATCH_SIZE,shuffle=True,pin_memory=True,num_workers=4)
 val_loader = DataLoader(val,batch_size=BATCH_SIZE,shuffle=False,pin_memory=True,num_workers=4)
-test_data = MNIST(root="data/MNIST",train=False,transform=tfm,download=True)
 test_loader = DataLoader(test_data,batch_size=BATCH_SIZE,shuffle=False,pin_memory=True,num_workers=4)
 
 ## Model
 class CNN(nn.Module):
     def __init__(self,image_channels):
         super().__init__()
-        self.conv_net = nn.Sequential(
-            nn.Conv2d(image_channels,image_channels*128,kernel_size=3,stride=3,padding=1),
+        self.conv_net_one = nn.Sequential(
+            nn.Conv2d(image_channels,image_channels*128,kernel_size=3,stride=3,padding=2),# 384 * 12 * 12
+            nn.BatchNorm2d(image_channels*128),
             nn.ReLU(),
             nn.Dropout(0.2),
-            nn.AvgPool2d(kernel_size=3,stride=3) # 128 * 3 * 3
+            nn.AvgPool2d(kernel_size=2,stride=1),  # 384 * 11 * 11
         )
-        self.linear_net = nn.Linear(128*3*3,NUM_CLASSES)
+        self.conv_net_two = nn.Sequential(
+            nn.Conv2d(image_channels*128,image_channels*128,kernel_size=3,stride=1,padding=1), # 384 * 11 * 11
+            nn.BatchNorm2d(image_channels*128),
+            nn.ReLU(),
+        )
+        self.linear_net = nn.Linear(384*11*11,NUM_CLASSES)
     def forward(self,x):
-        x =  self.conv_net(x)
-        x = x.view(x.size(0),-1)
-        x = self.linear_net(x)
-        return x
+        x =  self.conv_net_one(x)
+        y = torch.add(x,self.conv_net_two(x))
+        z = y.view(y.size(0),-1)
+        z = self.linear_net(z)
+        return z
     
 ## Training
 model = CNN(IMAGE_CHANNELS).to(device=DEVICE)
 optimizer = optim.Adam(model.parameters(),lr=LR)
 criterion = nn.CrossEntropyLoss()
 train_losses,val_losses = [],[]
-writer = SummaryWriter("runs/mnist_cnn")
+writer = SummaryWriter("runs/mnist_cifar")
 for ep in range(NUM_EPOCHS):
     running_loss, total_size, val_loss,val_acc, val_size = 0,0,0,0,0
     model.train()
@@ -92,6 +118,7 @@ for ep in range(NUM_EPOCHS):
     val_losses.append(val_loss/val_size)
 writer.close()
 
+## Plotting
 plt.figure(figsize=(10,8))
 plt.title("Losses")
 plt.xlabel("Epoch")
@@ -99,8 +126,9 @@ plt.ylabel("Loss")
 plt.plot(list(range(len(train_losses))),train_losses,label="Train")
 plt.plot(list(range(len(val_losses))),val_losses,label="Val")
 plt.legend()
-plt.savefig("results/plots/ffn_losses.png")
+plt.savefig("results/plots/cnn_cifar_losses.png")
 
+## Inference
 test_acc, total_size = 0, 0
 model.eval()
 with torch.no_grad():
